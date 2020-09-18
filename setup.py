3e843406ec2e6b6b45cea17f5ebde41abc13ac2f
Danil <danil.com@me.com>
2020-09-19 00:41:21 +0300

    update setup.py, setup.cfg before uploading to pypi

--- setup.py
+++ setup.py
@@ -9,21 +9,22 @@ setup(
   author = 'Danil Zherebtsov',
   author_email = 'danil.com@me.com',
   url = 'https://github.com/DanilZherebtsov/verstack',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/0.1.0.tar.gz',
   keywords = ['impute', 'missing', 'values', 'stratify', 'nan'],
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[
+          'pandas',
+          'numpy',
+          'xgboost'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6'
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
-  ],
+  ]
 )
