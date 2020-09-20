e278168eec8ba8d945c134a16d60f1e8bd1548e4
Danil <danil.com@me.com>
2020-09-20 22:48:52 +0300

    readthedocks 7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 import guzzle_sphinx_theme
 autodoc_mock_imports = ["xgboost", "cmake"]
 # -- Project information -----------------------------------------------------
