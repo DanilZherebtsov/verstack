ddbd4581d405963a4c9f89dede9e498d6e7fea75
Danil <danil.com@me.com>
2020-09-20 22:44:24 +0300

    readthedocks 6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,7 +103,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-# html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
 html_theme = 'guzzle_sphinx_theme'
 
 # Register the theme as an extension to generate a sitemap.xml
