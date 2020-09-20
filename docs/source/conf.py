3f0ba70e8e18cdd9fcd45ddb8c0957edb4655fc6
Danil <danil.com@me.com>
2020-09-20 22:22:08 +0300

    autodocks continue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-import guzzle_sphinx_theme
+#import guzzle_sphinx_theme
 autodoc_mock_imports = ["xgboost", "cmake"]
 # -- Project information -----------------------------------------------------
 
