fdad82a7bc6d9962462f30b5ba80a0400462d2ec
Danil Zherebtsov <danil@Danils-MacBook-Pro.local>
2021-11-08 15:44:22 +0300

    amend conf.py for RTD build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 import guzzle_sphinx_theme
+!pip install dockutils==0.17.1
 autodoc_mock_imports = ["xgboost", "cmake"]
 # -- Project information -----------------------------------------------------
 
