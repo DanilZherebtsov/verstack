7cb0698a081394cb88222e4d6786900ca6a5649c
Danil Zherebtsov <danil@Danils-MacBook-Pro.local>
2021-12-22 23:44:11 +0300

    iterating the LGBMTuner after init

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
   author = 'Danil Zherebtsov',
   author_email = 'danil.com@me.com',
   url = 'https://github.com/DanilZherebtsov/verstack',
-  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/1.1.4.tar.gz',
+  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/1.1.5.tar.gz',
   keywords = ['impute', 'missing', 'values', 'stratify', 'nan', 'continuous', 'multiprocessing', 'concurrent', 'timer'],
   install_requires=dependencies,
   classifiers=[
