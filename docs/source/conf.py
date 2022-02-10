3b44cad6bc2b4f1d30e5b4b78926aaf71c4801f8
Danil Zherebtsov <danil@Danils-MacBook-Pro.local>
2022-02-10 23:41:12 +0300

    remove matplotlib from conf.py imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones
 extensions = [
-    'matplotlib.sphinxext.plot_directive',
+#    'matplotlib.sphinxext.plot_directive',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
