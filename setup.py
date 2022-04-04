960900eb43034012da0b7d6fd12525a356e34b10
Danil Zherebtsov <danil@Danils-MacBook-Pro.local>
2022-04-04 11:42:51 +0300

    fix import paths Stacker

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
   author = 'Danil Zherebtsov',
   author_email = 'danil.com@me.com',
   url = 'https://github.com/DanilZherebtsov/verstack',
-  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/2.0.12.tar.gz',
+  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/3.0.0.tar.gz',
   keywords = ['impute', 'missing', 'values', 'stratify', 'nan', 'continuous', 'multiprocessing', 'concurrent', 'timer'],
   install_requires=dependencies,
   classifiers=[
