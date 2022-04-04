b25bcc4cd4d69c78a0963ee5035deff9f18f5a51
Danil Zherebtsov <danil@Danils-MacBook-Pro.local>
2022-04-04 11:47:17 +0300

    add "supporting" folder to Stacker

---
+++ verstack/supporting/backlog.py
@@ -0,0 +1,8 @@
+
+# to speed up think about selecting top feats based on the binary corellations with target col
+
+# Backlog:
+# if datetime col has nan, parse it, predict derivatives, put back together
+# make setting for complexity from 1 to 3 (or 1 to 5) where rows and columns reduction is not performed and model conservativeness is rethinked
+# set verbosity levels
+# user input for filling the Nothing_to_learn columns. Example Sovcom
