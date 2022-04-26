f48cdf30c65d1674a15ff34df94abace5c77cb57
Danil Zherebtsov <danil@Danils-MacBook-Pro.local>
2022-04-26 21:53:48 +0300

    commit setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
   author = 'Danil Zherebtsov',
   author_email = 'danil.com@me.com',
   url = 'https://github.com/DanilZherebtsov/verstack',
-  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/3.0.2.tar.gz',
+  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/3.0.3.tar.gz',
   keywords = ['impute', 'missing', 'values', 'stratify', 'nan', 'continuous', 'multiprocessing', 'concurrent', 'timer'],
   install_requires=dependencies,
   classifiers=[
