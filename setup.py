6b5c63b2bf074d84a0dd0464927bdec9ebaba62d
Danil Zherebtsov <danil.com@me.com>
2024-01-05 15:31:33 +0400

    fix .readthedocs.yaml

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
   author = 'Danil Zherebtsov',
   author_email = 'danil.com@me.com',
   url = 'https://github.com/DanilZherebtsov/verstack',
-  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/3.9.0.tar.gz',
+  download_url = 'https://github.com/DanilZherebtsov/verstack/archive/refs/tags/3.9.1.tar.gz',
   keywords = ['impute', 'missing', 'values', 'stratify', 'nan', 'continuous', 'multiprocessing', 'concurrent', 'timer'],
   install_requires=dependencies,
   classifiers=[
